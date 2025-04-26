20f52e7eafb74585079644824de4ec0f805b8135
szymonpilszak <szymon.pilszak@gmail.com>
2025-04-27 00:54:26 +0200

    test

--- build/test/dependencies/test_UDS_unit_runner.d
+++ build/test/dependencies/test_UDS_unit_runner.d
@@ -1,4 +1,4 @@
-build/gcov/out/test_UDS_unit/test_UDS_unit_runner.o: \
+build/test/out/test_UDS_unit/test_UDS_unit_runner.o: \
  build/test/runners/test_UDS_unit_runner.c build/vendor/unity/src/unity.h \
  build/vendor/unity/src/unity_internals.h build/vendor/cmock/src/cmock.h \
  build/vendor/cmock/src/cmock_internals.h src/uds_services.h src/config.h \
