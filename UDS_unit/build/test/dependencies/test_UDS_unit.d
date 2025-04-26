b40f60db6c4f307b9d4824a0cb6eab1b7d4b510b
szymonpilszak <szymon.pilszak@gmail.com>
2025-04-26 20:50:43 +0200

    Add gcovr for code coverage maessurrement

--- UDS_unit/build/test/dependencies/test_UDS_unit.d
+++ UDS_unit/build/test/dependencies/test_UDS_unit.d
@@ -1,4 +1,4 @@
-build/test/out/test_UDS_unit/test_UDS_unit.o: test/test_UDS_unit.c \
+build/gcov/out/test_UDS_unit/test_UDS_unit.o: test/test_UDS_unit.c \
  build/vendor/unity/src/unity.h build/vendor/unity/src/unity_internals.h \
  build/test/mocks/test_UDS_unit/mock_lin_transport.h src/lin_transport.h \
  src/config.h src/uds_services.h
