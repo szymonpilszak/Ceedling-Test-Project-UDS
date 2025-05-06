b3ffb426a46b23657540956cdcca9bae0b2d76e8
Szymon Pilszak <szymon.pilszak@swtest.pl>
2025-05-06 13:11:42 +0200

    clean up

--- build/test/dependencies/mock_lin_transport.d
+++ build/test/dependencies/mock_lin_transport.d
@@ -1,4 +1,4 @@
-build/test/out/test_uds_services/mock_lin_transport.o: \
+build/gcov/out/test_uds_services/mock_lin_transport.o: \
  build/test/mocks/test_uds_services/mock_lin_transport.c \
  build/vendor/cmock/src/cmock.h build/vendor/cmock/src/cmock_internals.h \
  build/vendor/unity/src/unity.h build/vendor/unity/src/unity_internals.h \
